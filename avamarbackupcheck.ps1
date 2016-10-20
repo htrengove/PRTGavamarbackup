e2d576330bbad4a8d1bcbb73e93ca224df942ccc
htrengove <heath.trengove@plusit.com.au>
2016-10-21 09:55:50 +1100

    Powershell Script for Custom EXE/ADV Sensor
    Script is used to monitor EMC Avamar clients backup status

---
+++ avamarbackupcheck.ps1
@@ -0,0 +1,27 @@
+﻿$DEVICE=$args[0]
+#Set Path
+$logpath = "\\$($DEVICE)\c$\Program Files\avs\var\clientlogs"
+cd $logpath
+#Get Latest Log
+$logfile = gci *.log | sort LastWriteTime | select -last 1 | Select-object -Property Name
+#Check Log
+$status = Select-String -Path $logfile.Name -Pattern "avtar Info <5314>:" |  Foreach {$_.Line}
+#remove unwanted chars
+$statusclean = $status -replace ‘[<>]’
+#check status outcome
+if ($status -match "Command failed") { $result= "1" } else { $result = "0" }
+#send to PRTG
+
+"<prtg>"
+"<result>"   
+"<channel>"
+"BackupStatus"
+"</channel>"
+"<value>"
+echo $result
+"</value>"
+"</result>"
+"<text>"
+echo "Status: $($statusclean)"
+"</text>"
+"</prtg>"
